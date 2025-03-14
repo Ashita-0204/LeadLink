cdcbfdce33789d9798c560a93ad2bdb206cba6b4
Yashraj1118 <yash.raj17182003@gmail.com>
2025-03-14 21:05:02 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div>
-      <header className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white py-5 px-8 shadow-lg flex justify-between items-center rounded-b-2xl">
-        <h1 className="text-2xl font-extrabold tracking-wide">LeadLink</h1>
+      <header className="bg-[#0B1623] text-white py-4 px-8 shadow-md flex justify-between items-center -b-lg">
+        <h1 className="text-2xl font-bold tracking-wide">LeadLink</h1>
         <nav>
           <ul className="flex space-x-6">
             <li>
               <Link
                 to="/landing"
-                className="text-lg font-medium hover:text-gray-200 transition-colors"
+                className="text-lg font-medium hover:text-gray-300 transition-colors"
               >
                 Dashboard
               </Link>
@@ -19,7 +19,7 @@ export default function Header() {
             <li>
               <Link
                 to="/list"
-                className="text-lg font-medium hover:text-gray-200 transition-colors"
+                className="text-lg font-medium hover:text-gray-300 transition-colors"
               >
                 Tickets
               </Link>
@@ -27,15 +27,15 @@ export default function Header() {
             <li>
               <Link
                 to="/tickets"
-                className="text-lg font-medium hover:text-gray-200 transition-colors"
+                className="text-lg font-medium hover:text-gray-300 transition-colors"
               >
                 All Tickets
               </Link>
             </li>
             <li>
               <Link
-                to="/logout"
-                className="text-lg font-medium hover:text-gray-200 transition-colors"
+                to="/"
+                className="text-lg font-medium hover:text-gray-300 transition-colors"
               >
                 Logout
               </Link>
