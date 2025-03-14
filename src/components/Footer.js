64d1c5e6c09a743897c5a1ea3fa8ec377475623c
Yashraj1118 <yash.raj17182003@gmail.com>
2025-03-15 00:23:46 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-        <footer className="bg-gray-900 text-gray-300 py-6 px-8 text-center shadow-inner rounded-t-2xl">
-      <p className="text-sm">&copy; {new Date().getFullYear()} LeadLink. Crafted with care for seamless connections.</p>
+      <footer className="bg-gray-900 text-gray-300 py-6 px-8 text-center shadow-inner -t-2xl w-full absolute bottom-0">
+       <p className="text-sm">&copy; {new Date().getFullYear()} LeadLink. Crafted with care for seamless connections.</p>
       <div className="mt-2 flex justify-center space-x-4">
         <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors">Facebook</a>
         <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="hover:text-white transition-colors">Twitter</a>
