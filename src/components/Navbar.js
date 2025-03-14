d2cd18b0f4b73c534a5b3cec5a0fd01be2353ae3
Yashraj1118 <yash.raj17182003@gmail.com>
2025-03-14 21:02:42 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
+
 import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div>
-      <header className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white py-5 px-8 shadow-lg flex justify-between items-center rounded-b-2xl">
-        <h1 className="text-2xl font-extrabold tracking-wide">LeadLink</h1>
+      <header className="bg-[#0B1623] text-white py-4 px-8 shadow-md flex justify-between items-center -b-lg">
+        <h1 className="text-2xl font-bold tracking-wide">LeadLink</h1>
         <nav>
           <ul className="flex space-x-6">
             <li>
               <Link
                 to="/"
-                className="text-lg font-medium hover:text-gray-200 transition-colors"
+                className="text-lg font-medium hover:text-gray-300 transition-colors"
               >
                 Home
               </Link>
@@ -19,7 +20,7 @@ export default function Navbar() {
             <li>
               <Link
                 to="/createuser"
-                className="text-lg font-medium hover:text-gray-200 transition-colors"
+                className="text-lg font-medium hover:text-gray-300 transition-colors"
               >
                 Signup
               </Link>
